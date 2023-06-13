62602b0adbb1780dbafb40e0716e3ba5985fa693
c.morais <c.morais1507@gmail.com>
2023-06-13 12:54:32 -0300

    :lipstick: feat: Determina o tema

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,13 +6,41 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+        violet1: '#fdfcfe',
+        violet2: '#fbfaff',
+        violet3: '#f5f2ff',
+        violet4: '#ede9fe',
+        violet5: '#e4defc',
+        violet6: '#d7cff9',
+        violet7: '#c4b8f3',
+        violet8: '#aa99ec',
+        violet9: '#6e56cf',
+        violet10: '#644fc1',
+        violet11: '#5746af',
+        violet12: '#20134b',
+        mauve1: '#fdfcfd',
+        mauve2: '#f9f8f9',
+        mauve3: '#f4f2f4',
+        mauve4: '#eeedef',
+        mauve5: '#e9e8ea',
+        mauve6: '#e4e2e4',
+        mauve7: '#dcdbdd',
+        mauve8: '#c8c7cb',
+        mauve9: '#908e96',
+        mauve10: '#86848d',
+        mauve11: '#6f6e77',
+        mauve12: '#1a1523',
     },
+    fontSize: {
+      h1: '44px',
+      h2: '36px',
+      h3: '28px',
+      h4: '20px',
+      text: '16px',
+      btn: '14px',
+      titles: '128px',
+    }
   },
   plugins: [],
 }
