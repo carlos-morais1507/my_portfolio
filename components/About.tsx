800c0e2ce05220bcb998323a5834427646b151a1
c.morais <c.morais1507@gmail.com>
2023-06-13 21:55:34 -0300

    :iphone: Problemas de responsividade consertados

--- components/About.tsx
+++ components/About.tsx
@@ -12,8 +12,8 @@ const About = () => {
         <h1 className='text-titles slide-t slide-full text-violet11 font-bold'>ABOUT ME • ABOUT ME • ABOUT ME • </h1>
         <h1 className='text-titles slide-t slide-full text-violet11 font-bold'>ABOUT ME • ABOUT ME • ABOUT ME • </h1>
       </div>
-      <div className='w-full h-[650px] border-b-2 border-violet12 flex flex-col md:flex-row overflow-hidden'>
-        <div className='w-full min-w-[50%] min-h-[50%] h-full md:border-r-2 border-b-2 md:border-b-0 border-violet12 bg-carlos-photo bg-cover bg-center flex items-end justify-end'>
+      <div className='w-full h-full md:h-[650px] border-b-2 border-violet12 flex flex-col md:flex-row overflow-hidden'>
+        <div className='w-full min-w-[50%] min-h-[325px] h-full md:border-r-2 border-b-2 md:border-b-0 border-violet12 bg-carlos-photo bg-cover bg-center flex items-end justify-end'>
           <div className="whitespace-nowrap overflow-hidden">
             <Image src="/assets/AboutBadge.svg" alt="About Badge" width={200} height={200} className="animate-spin-slow mb-[-30%] ml-[30%]"/>
           </div>
