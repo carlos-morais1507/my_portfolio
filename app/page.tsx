fb0724a0a904aabe1e409bf2e0a90f7bc3b330ad
c.morais <c.morais1507@gmail.com>
2023-06-14 10:55:29 -0300

    :sparkles: feat: Seção Skills criada

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import { About, Hero } from '@/components'
+import { About, Hero, Skills } from '@/components'
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-violet1 text-violet12 text-text">
       <Hero />
       <About />
+      <Skills />
     </main>
   )
 }
--- components/index.ts
+++ components/index.ts
@@ -1,9 +1,11 @@
 import Header from "./Header";
 import Hero from "./Hero";
 import About from "./About";
+import Skills from "./Skills";
 
 export {
   Header,
   Hero,
-  About
+  About,
+  Skills
 }
